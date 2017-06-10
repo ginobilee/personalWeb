cb0d52c2fc362b877fdb67996ce8465af90ee90a
ginobilee <ginobilee@users.noreply.github.com>
2017-06-11 00:19:38 +0800

    change port setting

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var express = require('express'),
     ejs = require('ejs');
 
 var app = express();
+app.set('port',(process.env.PORT || 80)); 
 
 	app.use(function(req,res){
 		res.end('hello');
 	});
 
-app.listen(process.env.port || 80,function(){
+app.listen(app.get('port'),function(){
 	console.log('The server is running now.');
 });
