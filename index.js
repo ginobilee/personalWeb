c953b8f5d9c045291c57f7091b65f8049d2eafdc
ginobilee <ginobilee@users.noreply.github.com>
2017-06-12 17:12:51 +0800

    add index.pug

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 app.set('views',__dirname+'/views');
 app.set('view engine','pug');
-app.set('port',(process.env.PORT || 80)); 
+app.set('port',(process.env.PORT || 8080)); 
+
+app.use(express.static(__dirname+'/public'));
 
 	app.use(function(req,res){
 		res.render('index');
