2c12e7ae96930bc8252cbed34cbd6260872cf420
ginobilee <ginobilee@users.noreply.github.com>
2017-06-21 16:24:19 +0800

    specify mongodb and contact route

--- contactRouter.js
+++ contactRouter.js
@@ -1,5 +1,9 @@
+'use strict';
+
 const express = require('express');
+const mongoose = require('mongoose');
 const contactRouter = express.Router();
+const uriDB = process.env.MONGODB_URI;
 exports = module.exports = contactRouter;
 
 contactRouter.get('',function(req,res){
@@ -8,6 +12,32 @@ contactRouter.get('',function(req,res){
 
 contactRouter.post('',function(req,res){
 	let m = req.body;
-	console.log(m);
+	let newMessage = new userMessage(m);
+	mongoose.connect(uriDB,process.env.mongodb_uri,function(err,res){
+		if(err){
+			console.log('ERROR connecting to:'+uriDB);
+		}else{
+			console.log('connected');
+			newMessage.save(function(err){
+				if(err){
+					console.log('ERROR saving:'+err);
+				}
+			});
+		}
+	});
+	//console.log(m);
 	res.json(m);
 });
+
+const messageSchema = new mongoose.Schema({
+	user_name:{
+		type:String
+	},
+	user_email:{
+		type:String
+	},
+	user_message:{
+		type:String
+	}
+});
+const userMessage = mongoose.model('userMessage',messageSchema);
